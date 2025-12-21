f89023d2a790e0d69cc947c0ffb68058f6fb8f30
aaltamimi2 <aaltamimi2@wisc.edu>
2025-12-21 17:25:53 -0600

    Expand common-solvent visualizations and outputs

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,14 @@
 """Machine learning models for SLE correction"""
+
+from .common_solvent_model import (  # noqa: F401
+    build_model_pipeline,
+    build_preprocessor,
+    ensemble_predict,
+    evaluate_models,
+    fit_ensemble,
+    load_common_solvent_csv,
+    load_common_solvent_directory,
+    run_common_solvent_active_learning,
+    run_common_solvent_cv_and_visualize,
+    rank_active_learning_batch,
+)
